7e08d538ac68987fd8e61d84bd0fa307c9bd5b05
taufique <taufiquehr@gmail.com>
2019-06-28 17:04:19 +0600

    make it better

--- deploytodotasker/settings.py
+++ deploytodotasker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.10/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
-import django_heroku
+#import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -184,4 +184,4 @@ try:
 except ImportError:
     pass
 
-django_heroku.settings(locals())
+
